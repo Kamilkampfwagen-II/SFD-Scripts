ec031d59843aefda5ace5ccc9526fe60c69bfff5
Kamilkampfwagen-II <84380947+Kamilkampfwagen-II@users.noreply.github.com>
2023-02-14 20:38:52 +0300

    Added smoke trail effect.

--- WaterExtinguishes/WaterExtinguishes.cs
+++ WaterExtinguishes/WaterExtinguishes.cs
@@ -1,19 +1,23 @@
-﻿using System;
+using System;
 using System.Linq;
 using System.Collections;
 using System.Collections.Generic;
 using System.Text;
 using SFDGameScriptInterface;
 
-namespace Flashbang {
+namespace WaterExtinguishes {
 public partial class GameScript : GameScriptInterface
 {
 public GameScript() : base(null) {}
 /* SCRIPT STARTS HERE - COPY BELOW INTO THE SCRIPT WINDOW */
 // Water Extinguishes
 // by EristeYasar
-private static bool WATER_EXTINGUISHES = true;
-private static bool ACID_EXTINGUISHES = true;
+
+// Settings:
+const bool WATER_EXTINGUISHES = true;
+const bool ACID_EXTINGUISHES = true;
+const float SMOKE_TIME_S = 3f;
+
 public static void OnStartup()
 {
     Events.UpdateCallback.Start(OnUpdate);
@@ -22,21 +26,24 @@ public static void OnUpdate(float delta_ms)
 {
     foreach (FireNode fireNode in Game.GetFireNodes())
     {
-        if (!((IsInWater(fireNode.Position) && WATER_EXTINGUISHES) || (IsInAcid(fireNode.Position) && ACID_EXTINGUISHES))) {continue;}
+        if (!((IsInWaterZone(fireNode.Position) && WATER_EXTINGUISHES) || (IsInAcidZone(fireNode.Position) && ACID_EXTINGUISHES))) {continue;}
+        Game.PlaySound("Splash", fireNode.Position);
         Game.EndFireNode(fireNode.InstanceID);
     }
+
     foreach (IObject burningObj in Game.GetBurningObjects())
     {
-        if (!((IsInWater(burningObj.GetWorldPosition()) && WATER_EXTINGUISHES) || (IsInAcid(burningObj.GetWorldPosition()) && ACID_EXTINGUISHES))) {continue;}
-        Game.PlayEffect("TR_SPR", burningObj.GetWorldPosition(), burningObj.UniqueID, "TR_S", 3f);
+        if (!((IsInWaterZone(burningObj.GetWorldPosition()) && WATER_EXTINGUISHES) || (IsInAcidZone(burningObj.GetWorldPosition()) && ACID_EXTINGUISHES))) {continue;}
+        Game.PlayEffect("TR_SPR", burningObj.GetWorldPosition(), burningObj.UniqueID, "TR_S", SMOKE_TIME_S);
+        Game.PlaySound("Splash", burningObj.GetWorldPosition());
         burningObj.ClearFire();
     }
 }
-private static bool IsInWater(Vector2 pos)
+private static bool IsInWaterZone(Vector2 pos)
 {
     return Game.GetObjectsByName("WaterZone").Any(waterZone => waterZone.GetAABB().Contains(pos));
 }
-private static bool IsInAcid(Vector2 pos)
+private static bool IsInAcidZone(Vector2 pos)
 {
     return Game.GetObjectsByName("AcidZone").Any(acidZone => acidZone.GetAABB().Contains(pos));
 }
